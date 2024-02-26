862f0271756d14f888297ac3a4ff29ee117cb22e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-26 02:31:07 -0700

    import Routes into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/Home.js";
 
 function App() {
   return (
-    <div >
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
