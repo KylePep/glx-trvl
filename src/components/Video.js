b03a821fb0eb02338953140362d9f09f3c2c4ef3
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-28 06:13:14 -0700

    imported assests, created video component and video css

---
+++ src/components/Video.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './VideoStyles.css'
+
+import spaceVideo from '../assets/space.mp4'
+import { Link } from "react-router-dom"
+
+const Video = () => {
+  return (
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={spaceVideo} type="video/mp4"/>
+      </video>
+      <div className="content">
+        <h1>Galaxy. Travel.</h1>
+        <p>Worlds first civilian space travel.</p>
+      </div>
+      <div>
+        <Link to='/training' className='btn'> Training</Link>
+        <Link to='/training' className='btn btn-light'> Launch</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Video
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from "../components/Navbar.js"
+import Video from "../components/Video.js"
 
 const Home = () => {
   return (
     <div>
-
       <Navbar />
+      <Video/>
     </div>
   )
 }
