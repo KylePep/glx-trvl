ed7bfb6d18ea533cc87ff930d24f6a389664252f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-28 04:59:41 -0700

    added eslintrc.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './NavbarStyles.css'
 
 const Navbar = () => {
   return (
-    <div className="header">
-
+    <div className='header'>
+      <Link to='/'>
+        <h1>GLX TRVL</h1>
+      </Link>
     </div>
-
   )
 }
 
