6c7d1a015cdf49fd07999d8298e9b3b810f7f12e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-28 06:51:23 -0700

    Footer and footer styling

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "../components/Navbar.js"
 import Video from "../components/Video.js"
+import Footer from "../components/Footer.js"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Video/>
+      <Footer/>
     </div>
   )
 }
