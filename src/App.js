b966bfc42071a63af5edc2c84461b833a99853be
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-28 08:23:10 -0700

    Form component and styling, Contact page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home.js";
 import Pricing from "./routes/Pricing.js";
 import Training from "./routes/Training.js";
+import Contact from "./routes/Contact.js";
+// import Footer from "./components/Footer.js";
+// import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <>
+    {/* <Navbar/> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/pricing" element={<Pricing/>}/>
         <Route path="/training" element={<Training/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
+      {/* <Footer/> */}
     </>
   );
 }
